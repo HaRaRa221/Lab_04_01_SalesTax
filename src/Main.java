0699cccd61f1b5ae3b1a97959f8b57f7f1f60672
Raymond Ha <Haro@mail.uc.edu>
2022-09-18 20:39:46 -0400

    Added itemPurchase, salesTax, and totalCost to output

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int itemPurchase = 0;
+        int salesTax;
+        
+
+        salesTax = (int) (itemPurchase *.05);
+        itemPurchase = 45;
+        System.out.println("The cost of item is " + itemPurchase);
+        System.out.println("The sales tax of item is " + salesTax);
+
+
+
+
     }
 }
